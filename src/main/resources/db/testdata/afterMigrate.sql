f70d7c62e7a391297f0f23fdf2f0340a2107bb7b
Alex Holanda <alex.holanda@outlook.com>
2021-01-05 17:51:26 -0300

    Desabilitado o flyway e modificado a linguagem do hibernate para mysql5

--- src/main/resources/db/testdata/afterMigrate.sql
+++ src/main/resources/db/testdata/afterMigrate.sql
@@ -6,22 +6,22 @@ truncate table tb_order_product;
 
 set foreign_key_checks = 1;
 
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Pizza Bacon', 49.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg', 'Pizza de bacon com mussarela, orégano, molho especial e tempero da casa.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Pizza Moda da Casa', 59.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_moda.jpg', 'Pizza à moda da casa, com molho especial e todos ingredientes básicos, e queijo à sua escolha.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Pizza Portuguesa', 45.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_portuguesa.jpg', 'Pizza Portuguesa com molho especial, mussarela, presunto, ovos e especiarias.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Risoto de Carne', 52.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_carne.jpg', 'Risoto de carne com especiarias e um delicioso molho de acompanhamento.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Risoto Funghi', 59.95, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_funghi.jpg', 'Risoto Funghi feito com ingredientes finos e o toque especial do chef.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Macarrão Espaguete', 35.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_espaguete.jpg', 'Macarrão fresco espaguete com molho especial e tempero da casa.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Macarrão Fusili', 38.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_fusili.jpg', 'Macarrão fusili com toque do chef e especiarias.');
-INSERT INTO tb_product (name, price, image_Uri, description) VALUES ('Macarrão Penne', 37.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_penne.jpg', 'Macarrão penne fresco ao dente com tempero especial.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (1, 'Pizza Bacon', 49.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_bacon.jpg', 'Pizza de bacon com mussarela, orégano, molho especial e tempero da casa.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (2, 'Pizza Moda da Casa', 59.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_moda.jpg', 'Pizza à moda da casa, com molho especial e todos ingredientes básicos, e queijo à sua escolha.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (3, 'Pizza Portuguesa', 45.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/pizza_portuguesa.jpg', 'Pizza Portuguesa com molho especial, mussarela, presunto, ovos e especiarias.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (4, 'Risoto de Carne', 52.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_carne.jpg', 'Risoto de carne com especiarias e um delicioso molho de acompanhamento.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (5, 'Risoto Funghi', 59.95, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/risoto_funghi.jpg', 'Risoto Funghi feito com ingredientes finos e o toque especial do chef.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (6, 'Macarrão Espaguete', 35.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_espaguete.jpg', 'Macarrão fresco espaguete com molho especial e tempero da casa.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (7, 'Macarrão Fusili', 38.0, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_fusili.jpg', 'Macarrão fusili com toque do chef e especiarias.');
+INSERT INTO tb_product (id, name, price, image_Uri, description) VALUES (8, 'Macarrão Penne', 37.9, 'https://raw.githubusercontent.com/devsuperior/sds2/master/assets/macarrao_penne.jpg', 'Macarrão penne fresco ao dente com tempero especial.');
 
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (1, -22.946779, -43.217753, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (0, -25.439787, -49.237759, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (1, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
-INSERT INTO tb_order (status, latitude, longitude, address, moment) VALUES (0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (1, 0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (2, 1, -22.946779, -43.217753, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (3, 0, -25.439787, -49.237759, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (4, 0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (5, 1, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (6, 0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
+INSERT INTO tb_order (id, status, latitude, longitude, address, moment) VALUES (7, 0, -23.561680, -46.656139, 'Avenida Paulista, 1500', date_sub(sysdate(), interval 1 day));
 
 INSERT INTO tb_order_product (order_id, product_id) VALUES (1 , 1);
 INSERT INTO tb_order_product (order_id, product_id) VALUES (1 , 4);
